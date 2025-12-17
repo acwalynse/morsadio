365ac39bcf0b209cef58ab781229d0267841bcb6
Acwalynse <acwalynse@protonmail.com>
2025-12-17 12:45:29 +0300

    replaced Regex with native checks

--- src/track.rs
+++ src/track.rs
@@ -1,13 +1,19 @@
 use std::{thread, time::Duration};
 use actually_beep::beep_with_hz_and_millis;
-use regex::Regex;
 
+/// Parses a plain text with morse code to a playable track.
+/// Returns a vector of tuples that can be used in `track::play(Vec<(u32, u32)>)` or an error
+/// string.
 pub fn parse(track: &str, hz: u32, short_ms: u32, long_ms: u32) -> Result<Vec<(u32, u32)>, String> {
+    {
+        // Check if track doesn't contain illegal letters.
+        let dict: String = String::from(".-_/ ");
+        for c in track.chars() {
+            if !dict.contains(c) { return Err(String::from("Track contains non-morse symbols")) }
+        }
+    }
+    
     let mut parsed: Vec<(u32, u32)> = Vec::new();
-    
-    let reg: Regex = Regex::new(r"[^\.\-_\s/]").unwrap();
-    if reg.is_match(track) { return Err(String::from("Track contains non-morse symbols")) }
-    
     for unit in track.chars() {
         match unit {
             '.'         => parsed.push((hz, short_ms)),
@@ -21,6 +27,8 @@ pub fn parse(track: &str, hz: u32, short_ms: u32, long_ms: u32) -> Result<Vec<(u
     Ok(parsed)
 }
 
+/// Plays a sound from vector of tuples.
+/// Tuple should contain hertz as a first item and millis as a second one.
 pub fn play(track: Vec<(u32, u32)>) {
     for unit in track.iter() {
         if unit.0 == 0 {
